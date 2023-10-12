9846552adfd74c56858d3757ab8674236c503643
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2023-10-12 23:47:28 +0100

    Prisma, Mongodb and Auth deployed

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
+import axios from 'axios';
+import { signIn } from 'next-auth/react'
+import { useRouter } from "next/router";
 
 const Auth = () => {
+    const router = useRouter();
     const [email, setEmail] = useState("")
     const [name, setName] = useState("")
     const [password, setPassword] = useState("")
@@ -10,13 +15,44 @@ const Auth = () => {
 
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
-    }, [])
+    }, []);
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+        } catch (error) {
+            console.log('error')
+        }
+    }, [email, password, router])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+            login();
+        } catch (error) {
+            console.log('error')
+        }
+
+    }, [email, name, password, login]);
+
+
 
     return (
         <div className='relative h-full w-full bg-[url("/images/hero.jpg")] bg-no-repeat bg-center bg-fixed bg-cover '>
             <div className='bg-black w-full h-full lg:bg-opacity-50'>
                 <nav className='py-5 px-12'>
-                    <img src='/images/logo.png' alt='logo' className='h-12' />
+                    <Image width={148} height={48} src='/images/logo.png' alt='logo' />
                 </nav>
                 <div className="flex justify-center" >
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full  " >
@@ -47,15 +83,16 @@ const Auth = () => {
                                 type="password"
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition ">
-                            {variant === 'login'? 'Login' :'Sign up'}
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition ">
+                            {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
-                           {variant ==='login'? 'First time using Netflix?': 'Already have an account?'}
+                            {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+
                             <span onClick={toggleVariant} className="text-white ml-2 hover:underline cursor-pointer" >
-                                
-                                {variant ==='login'? 'Create an account': 'Sign in'}
+                                {variant === 'login' ? 'Create an account' : 'Sign in'}
                             </span>
+
                         </p>
                     </div>
                 </div>
