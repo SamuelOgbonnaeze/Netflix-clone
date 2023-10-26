ae49c17e89513fab5788e93044957e613fb03f04
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2023-10-26 02:51:14 +0100

    Movie card and Hover effects

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard"
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import MoviesList from "@/components/MoviesList";
+import useMoviesList from "@/hooks/useMoviesList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,12 +23,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-
+  const { data: movies = [] } = useMoviesList();
 
   return (
     <>
-     <Navbar />
-     <Billboard />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MoviesList title="Trending now" data={movies} />
+      </div>
     </>
   )
 }
