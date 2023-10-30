a3d6823a922ff6e4493fae4d2eb9e2e2651496fa
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2023-10-30 04:10:38 +0100

    InfoModal added

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BsFillPlayFill } from 'react-icons/bs';
+import { BiChevronDown } from 'react-icons/bi';
 import FavoriteButton from './FavoriteButton';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
+import useInfoModal from '@/hooks/useInfoModal';
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -10,6 +12,7 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
 
     const router = useRouter();
+    const { openModal } = useInfoModal();
 
     return (
         <div className='group bg-zinc-900 col-span relative h-[12vw]'>
@@ -19,9 +22,15 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                 <div className='z-10 bg-zinc-800 p-2 lg:p-4 absolute transition w-full shadow-md rounded-b-md'>
                     <div className='flex flex-row items-center gap-3 '>
                         <div className='w-6 h-6 lg:w-10 lg:h-10 cursor-pointer bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300' onClick={() => router.push(`/watch/${data?.id}`)}>
-                            <BsFillPlayFill  size={30} className='flex items-center' />
+                            <BsFillPlayFill size={30} className='flex items-center' />
                         </div>
                         <FavoriteButton movieId={data?.id} />
+                        <div 
+                        onClick={() => openModal(data?.id)}
+                        className='cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300 '>
+                            <BiChevronDown size={30} className='text-white group-hover/item:text-neutral-300 w-4 ' />
+                        </div>
+
                     </div>
                     <p className='text-green-400 font-semibold mt-4'>
                         New
@@ -41,4 +50,4 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
     )
 }
 
-export default MovieCard
+export default MovieCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import { getSession } from "next-auth/react";
 import MoviesList from "@/components/MoviesList";
 import useMoviesList from "@/hooks/useMoviesList";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
+import InfoModal from "@/components/InfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -26,16 +28,18 @@ export async function getServerSideProps(context: NextPageContext) {
 export default function Home() {
   const { data: movies = [] } = useMoviesList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
         <MoviesList title="Trending now" data={movies} />
         <MoviesList title="My List" data={favorites} />
       </div>
-     
+
     </>
   )
 }
